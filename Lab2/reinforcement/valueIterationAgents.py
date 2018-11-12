03106f8b1af685ab37008bcd357a92380ac5a57a
Roger <roger.caritj01@estudiant.upf.edu>
2018-11-12 21:03:51 +0100

    Q1

--- Lab2/reinforcement/valueIterationAgents.py
+++ Lab2/reinforcement/valueIterationAgents.py
@@ -45,17 +45,21 @@ class ValueIterationAgent(ValueEstimationAgent):
 
         # Write value iteration code here
         "*** YOUR CODE HERE ***"
+        #Assign the values in a new variable
         self.oldValues = self.values.copy()
         states = mdp.getStates()
+        #For every iteration we check every state and the possible actions
         for i in range(iterations):
             for state in states:
                 actions = mdp.getPossibleActions(state)
+                #While this state is not the last we look for the best action value
                 if not mdp.isTerminal(state):
-                    actionValue = -999999
+                    action_value = -999999
                     for action in actions:
-                        qValue = self.computeQValueFromValues(state, action)
-                        actionValue = max(actionValue, qValue)
-                    self.values[state] = actionValue
+                        q_value = self.computeQValueFromValues(state, action)
+                        action_value = max(action_value, q_value)
+                    #Set his own state as the best action value found
+                    self.values[state] = action_value
             self.oldValues = self.values.copy()
 
 
@@ -73,11 +77,15 @@ class ValueIterationAgent(ValueEstimationAgent):
           value function stored in self.values.
         """
         "*** YOUR CODE HERE ***"
-        qValue = 0
+        #Initial value to 0 (Has not started, no rewards, no points)
+        q_value = 0
         for nextState, prob in self.mdp.getTransitionStatesAndProbs(state, action):
-            qValue += prob * (self.discount * self.oldValues[nextState] + self.mdp.getReward(state, action, nextState))
-        return qValue
-        util.raiseNotDefined()
+            #For every transition we upgrade the q_value. We store there the cost of staying alive one more time per the
+            #cost of the new movement plus geting the reward. All of this multiplied by the probability of this event to 
+            #happen.
+            q_value += prob * (self.discount * self.oldValues[nextState] + self.mdp.getReward(state, action, nextState))
+        #Return the total q_value    
+        return q_value
 
     def computeActionFromValues(self, state):
         """
@@ -89,14 +97,22 @@ class ValueIterationAgent(ValueEstimationAgent):
           terminal state, you should return None.
         """
         "*** YOUR CODE HERE ***"
-        actions, maxValue, decision = self.mdp.getPossibleActions(state), -float('inf'), None
+        #We first take the possibles actions that we can do
+        actions = self.mdp.getPossibleActions(state)
+        #Set the value to a negative number so it can be easily updated
+        maxReward = -999999 
+        #In the beginning there is no decision made
+        decision = None
+        #We check the possible actions and then compare it with the maxReward
         for action in actions:
-            actionValue = self.computeQValueFromValues(state, action)
-            if actionValue > maxValue:
-                maxValue = actionValue
+            actionReward = self.computeQValueFromValues(state, action)
+            if actionReward > maxReward:
+                #If the reward is bigger than the former maximum, now this is the new maximum and the
+                #final decision is to take this action
+                maxReward = actionReward
                 decision = action
+        #We return the actions with the best reward        
         return decision
-        util.raiseNotDefined()
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
